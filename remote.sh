8a8c0e4581f4fa3a412aae849b980ec6cb526094
Mark Rogers <markrogersjr@gmail.com>
2020-02-10 21:01:52 -0800

    add some documentation

--- remote.sh
+++ remote.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# set up a cron job as follows:
+#   1. run `crontab -e`
+#   2. add a line to crontab file with command
+#        `sudo bash $HOME/financier/remote.sh`
 cd $HOME/financier
 source remote.rc
 rm -f results
