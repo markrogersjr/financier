61f3201d61126c7fe6295d626668196aff8f5274
Mark Rogers <markrogersjr@gmail.com>
2020-02-28 07:55:28 -0800

    fix remote.sh indentation

--- remote.sh
+++ remote.sh
@@ -7,9 +7,9 @@ cd $HOME/financier
 source env.rc
 rm -f results
 python3.8 main.py --subscriptions_filename $SUBSCRIPTIONS \
-									--plaid_credentials_filename $PLAID_CREDS \
-									--bank_credentials_filename $BANK_CREDS \
-									--from_email $FROM_EMAIL \
-									--to_email $TO_EMAIL
+                  --plaid_credentials_filename $PLAID_CREDS \
+                  --bank_credentials_filename $BANK_CREDS \
+                  --from_email $FROM_EMAIL \
+                  --to_email $TO_EMAIL
 chmod 777 results
 cat results | sendmail -f $FROM_EMAIL -t $TO_EMAIL
